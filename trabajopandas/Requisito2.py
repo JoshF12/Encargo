d9f54ec608ed4a73331377726f8e7dd40f8d2d8f
JoshF12 <jfloresri@utem.cl>
2025-09-02 22:36:09 -0400

    Update Requisito2.py

--- trabajopandas/Requisito2.py
+++ trabajopandas/Requisito2.py
@@ -1,16 +1,13 @@
 # cuantos estudiantes tienen todas las notas >= 4.0
 
 import pandas as pd
-from Estudiantes import estudiantes
+from Estudiantes import estudiantes, limpiar_notas
 
-df = pd.DataFrame(estudiantes)
+notas_limpias = limpiar_notas(estudiantes)
 
-# Filtrar estudiantes que tengan exactamente 3 notas
-df = df[df["notas"].apply(lambda x: isinstance(x, list) and len(x) == 3)]
-
-if df.empty:
-    print("No hay estudiantes con la cantidad correcta de notas.")
+if notas_limpias.empty:
+    print("No hay estudiantes con notas válidas.")
 else:
-    df["aprobados"] = df["notas"].apply(lambda x: all(n >= 4.0 for n in x))
-    cantidad_aprobados = df["aprobados"].sum()
-    print("Cantidad de aprobados (todas las notas>=4,0):",cantidad_aprobados)
+    aprobados = notas_limpias.apply(lambda fila: all(n >= 4.0 for n in fila if pd.notnull(n)), axis=1)
+    cantidad_aprobados = aprobados.sum()
+    print("Cantidad de aprobados (todas las notas >= 4,0):",cantidad_aprobados)
